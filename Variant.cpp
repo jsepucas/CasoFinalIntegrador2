518dfddae4b9751a8ea4fd5e382880403db201a3
jsepucas <jsepucas@myuax.com>
2023-11-28 12:15:02 +0100

    5

--- Variant.cpp
+++ Variant.cpp
@@ -1,2 +1,3 @@
-
 #include "Variant.h"
+
+Variant::Variant() : value(0) {}
--- Variant.h
+++ Variant.h
@@ -10,9 +10,20 @@
 
 class Variant {
 public:
-    using ProcType = std::function<Variant(const std::vector<Variant>&)>;
+
+    using ProcType = std::function<Variant(const std::vector<Variant>&)>
+            ;
 private:
+
     std::variant<int, float, std::string, std::vector<Variant>, ProcType> value;
+
+public:
+    Variant();
+    std::string to_string() const;
+    std::string to_json_string() const;
+    static Variant from_json_string(const std::string& json);
+    void parse_json(const std::string& json);
+
 
 };
 
