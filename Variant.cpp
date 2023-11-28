0f5eb8055a31b41bcca95c68909cea395ff5aee0
jsepucas <jsepucas@myuax.com>
2023-11-28 12:18:44 +0100

    8

--- Variant.cpp
+++ Variant.cpp
@@ -11,3 +11,11 @@ std::string Variant::to_string() const{
 std::string Variant::to_json_string() const{
 
 }
+
+std::string Variant::from_json_string(const std::string &json) {
+
+}
+
+void Variant::parse_json(const std::string &json) {
+
+}
