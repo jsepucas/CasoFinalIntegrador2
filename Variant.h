b70d37aecd35323ead67435d24e28415cc3abd9e
jsepucas <jsepucas@myuax.com>
2023-11-28 12:07:47 +0100

    3

---
+++ Variant.h
@@ -0,0 +1,16 @@
+#ifndef CASOFINALINTEGRADOR2_VARIANT_H
+#define CASOFINALINTEGRADOR2_VARIANT_H
+
+#include <string>
+#include <vector>
+#include <variant>
+#include <functional>
+
+
+
+class Variant {
+
+};
+
+
+#endif //CASOFINALINTEGRADOR2_VARIANT_H
